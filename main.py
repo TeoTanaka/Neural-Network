fd0ea54f3754821d6b31d5f11257220730f3b8f1
TeoTanaka <144719988+TeoTanaka@users.noreply.github.com>
2023-10-04 16:38:14 +0000

    1

---
+++ main.py
@@ -0,0 +1,26 @@
+training = ["hello","how","are","you","I","am","doing","well"]
+training_dict = {}
+#N GRAM LANGUAGE MODEL
+
+#BREAK APART training and itemize each word with the word that comes before it
+for a in training:
+    if training.index(a) > 0: #for every word after index 0
+        training_dict[a] = training[training.index(a)-1]
+
+#training dictionary is every letter : the letter before itself
+
+#GETTING INPUT
+print("PARROT: Hello there! How can I help you today?")
+inp = input("YOU: ")
+inp_split = inp.split()
+last_inp = inp_split[-1]#last letter of the input
+
+#IF the last letter of the input == to something in the training data
+
+
+
+
+
+    
+
+
